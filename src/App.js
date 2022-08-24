123ae0a7c15e281b9d4ed8b4040a3de6edb84c9e
eyewhysee <eyewhysee@gmail.com>
2022-08-24 14:51:07 +0000

    7th edit from draft/lingering-dust branch

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ function App() {
         <li>Fourth edit from the draft/lingering-dust fork</li>
         <li>Fifth edit from draft/lingering-dust branch: after manual pull request and then reload in CodeSandbox</li>
         <li>6th edit from draft/lingering-dust branch: Fifth edit had CodeSandbox auto link to PR</li>
+        <li>7th edit from draft/lingering-dust branch</li>
       </ul>
       <hr />
     </>
