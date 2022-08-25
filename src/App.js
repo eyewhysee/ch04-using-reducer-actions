1d73fb6c8a62c46475036c341090cebe5b0809ce
eyewhysee <eyewhysee@gmail.com>
2022-08-25 18:53:34 +0000

    From Gitpod: after installing vim extension

--- src/App.js
+++ src/App.js
@@ -79,6 +79,7 @@ function App() {
         <li>2nd of 3 edits to form a pull request from draft/lingering-dust branch</li>
         <li>3rd of 3 edits to form a pull request from draft/lingering-dust branch</li>
         <li>Gitpod edit on draft/lingering-dust branch</li>
+        <li>Gitpod: after installing vscodevim VS Code extension</li>
       </ul>
       <hr />
     </>
